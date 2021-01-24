ef36f453fc9e1d71a19deac35932c1a6394bef29
superestos <superestos@gmail.com>
2021-01-24 13:29:13 +0800

    flip edge

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -78,6 +78,66 @@ namespace CGL
   {
     // TODO Part 4.
     // TODO This method should flip the given edge and return an iterator to the flipped edge.
+
+    if (e0->isBoundary()) {
+      return e0;
+    }
+
+    auto h0 = e0->halfedge();
+    auto h1 = h0->next();
+    auto h2 = h1->next();
+    auto h3 = h0->twin();
+    auto h4 = h3->next();
+    auto h5 = h4->next();
+    auto h6 = h1->twin();
+    auto h7 = h2->twin();
+    auto h8 = h4->twin();
+    auto h9 = h5->twin();
+
+    auto v0 = h0->vertex();
+    auto v1 = h3->vertex();
+    auto v2 = h2->vertex();
+    auto v3 = h5->vertex();
+
+    auto e1 = h1->edge();
+    auto e2 = h2->edge();
+    auto e3 = h4->edge();
+    auto e4 = h5->edge();
+
+    auto f0 = h0->face();
+    auto f1 = h3->face();
+
+    h0->next() = h1;
+    h0->twin() = h3;
+    h0->vertex() = v3;
+    h0->edge() = e0;
+    h0->face() = f0;
+
+    h0->setNeighbors(h1, h3, v3, e0, f0);
+    h1->setNeighbors(h2, h7, v2, e2, f0);
+    h2->setNeighbors(h0, h8, v0, e3, f0);
+    h3->setNeighbors(h4, h0, v2, e0, f1);
+    h4->setNeighbors(h5, h9, v3, e4, f1);
+    h5->setNeighbors(h3, h6, v1, e1, f1);
+    h6->setNeighbors(h6->next(), h5, v2, e1, h6->face());
+    h7->setNeighbors(h7->next(), h1, v0, e2, h7->face());
+    h8->setNeighbors(h8->next(), h4, v3, e3, h8->face());
+    h9->setNeighbors(h9->next(), h5, v1, e4, h9->face());
+
+    v0->halfedge() = h2;
+    v1->halfedge() = h5;
+    v2->halfedge() = h3;
+    v3->halfedge() = h0;
+
+    e0->halfedge() = h0;
+    e1->halfedge() = h5;
+    e2->halfedge() = h1;
+    e3->halfedge() = h2;
+    e4->halfedge() = h4;
+
+    f0->halfedge() = h0;
+    f1->halfedge() = h3;
+
     return e0;
   }
 
